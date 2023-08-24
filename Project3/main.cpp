5676f355302cbcf2fb7b2ebce71bab91cbfadd5e
ducquy1996 <briannguyen0417@gmail.com>
2023-08-24 11:08:39 -0700

    Initial Commit

---
+++ Project3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project3
+//
+//  Created by Brian Nguyen on 8/24/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
