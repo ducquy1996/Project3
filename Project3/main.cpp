ed5885d7e9c36e1f746e0ffc212b4083b450cc8f
ducquy1996 <briannguyen0417@gmail.com>
2023-08-24 11:12:23 -0700

    Demo

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,14 +1,8 @@
-//
-//  main.cpp
-//  Project3
-//
-//  Created by Brian Nguyen on 8/24/23.
-//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+int main() {
+   
     std::cout << "Hello, World!\n";
     return 0;
 }
